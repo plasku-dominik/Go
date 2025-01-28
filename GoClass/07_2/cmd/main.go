3be37d5a4886b5b1213b498a30332e72ca0c5da7
DMNK <plasku.dominik1217@gmail.com>
2025-01-28 19:09:47 +0100

    error handling, word count

--- GoClass/07_2/cmd/main.go
+++ GoClass/07_2/cmd/main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	//"io"
+	"bufio"
 	"fmt"
-	"io"
 	"os"
+	"strings"
 )
 
 func main() {
 	for _, fname := range os.Args[1:] {
+		var lc, wc, cc int
+
 		file, err := os.Open(fname)
 
 		if err != nil {
@@ -15,11 +19,26 @@ func main() {
 			continue
 		}
 
-		if _, err := io.Copy(os.Stdout, file); err != nil {
+		/*data, err := io.ReadAll(file) // ioutil went outdated compared to the guide, io handles it
+
+		if err != nil {
 			fmt.Fprint(os.Stderr, err)
 			continue
 		}
 
+		fmt.Println("The file has", len(data), "bytes")*/
+
+		scan := bufio.NewScanner(file)
+
+		for scan.Scan() {
+			s := scan.Text()
+
+			wc += len(strings.Fields(s))
+			cc += len(s)
+			lc++
+		}
+
+		fmt.Printf(" %7d %7d %7d %s\n", lc, wc, cc, fname) // line, word, character
 		file.Close()
 	}
 }
